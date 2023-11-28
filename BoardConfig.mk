07b882bb88b190fb9de3430f819339060194d3b0
Leo <leohuangyi@foxmail.com>
2023-11-29 01:20:32 +0800

    Support lineage.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,6 +174,7 @@ TARGET_BOARD_PLATFORM := msmnile
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/wake_gesture"
 
 # Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
